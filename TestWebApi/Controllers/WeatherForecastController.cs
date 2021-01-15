548750c992d329543558273c0946cd777e12ac23
Fredrick Strøm <fredrick.strom@visma.com>
2021-01-15 11:21:22 +0100

    Added test project

---
+++ TestWebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Mime;
+using System.Threading.Tasks;
+
+namespace TestWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            var contentTypeWithoutName = new ContentType("application/json");
+            var contentTypeWithName = new ContentType("application/json") { Name = "test" };
+
+            var withoutName = TryValidateModel(contentTypeWithoutName);
+            ModelState.Clear();
+            var withName = TryValidateModel(contentTypeWithName);
+
+            return @"";
+        }
+    }
+}
